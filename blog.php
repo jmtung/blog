f5d6543bb9bd28d318caaa00da302b73429c801b
锦明 <1796932482@qq.com>
2019-06-02 23:43:50 +0800

    Site updated: 2019-06-02 23:43:50

--- blog.php
+++ blog.php
@@ -4,7 +4,7 @@ $data = json_decode($json);
 if (empty($data->ref)) {
     file_put_contents('/var/www/html/log/blog.log', 'type: test  '.'status: success  time: '.date('Y年m月d日 H:i:s',time()).PHP_EOL,FILE_APPEND) or die('error');
 } else {
-    if ($data->token == '006542') {
+    if ($data->token == '123456') {
         $command = "sudo git --git-dir='/var/www/blog/.git' --work-tree='/var/www/blog' pull";
         $exec_res = shell_exec($command);
         if ($exec_res!=''){
